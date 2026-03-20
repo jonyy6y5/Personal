78eedec60cb4f05f0e6d920bef7dff82bfc70ee1
jonyy6y5 <candiaj571@gmail.com>
2026-03-19 18:03:43 -0600

    se mandaron dos documentos nuevos

---
+++ EdadIf.js
@@ -0,0 +1,8 @@
+let edad = 20
+
+if (edad>=18){
+console.log( "es mayor de edad ")
+
+}else {
+    console.log("eres menor de edad")
+}
---
+++ paroimpar.js
@@ -0,0 +1,8 @@
+let numero= 3
+
+if (numero%2===0){
+   console.log("es par ")
+
+}else {
+    console.log("no es par")
+}
